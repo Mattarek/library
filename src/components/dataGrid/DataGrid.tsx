e2a4cc26d7fd1769806311a354de373477db5229
Marcin Barc <marb637@gmail.com>
2024-04-26 15:25:26 +0200

    datagrid added

--- src/components/dataGrid/DataGrid.tsx
+++ src/components/dataGrid/DataGrid.tsx
@@ -2,22 +2,70 @@ import { DataGrid } from "@mui/x-data-grid";
 import { useDemoData } from "@mui/x-data-grid-generator";
 
 export default function PageSizeCustomOptions() {
-  const { data } = useDemoData({
-    dataSet: "Commodity",
-    rowLength: 100,
-    maxColumns: 6,
+  const [pageState, setPageState] = useState({
+    isLoading: false,
+    data: [],
+    total: 0,
+    page: 1,
+    pageSize: 10,
   });
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setPageState((old) => ({ ...old, isLoading: true }));
+      try {
+        const response = await axios.get(
+          `https://demo.api-platform.com/admin/books?page=${pageState.page}&itemsPerPage=${pageState.pageSize}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        setPageState((old) => ({
+          ...old,
+          isLoading: false,
+          data: response.data["hydra:member"].map((book, index) => ({
+            ...book,
+            id: index + 1,
+          })),
+          total: response.data["hydra:totalItems"],
+        }));
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        setPageState((old) => ({ ...old, isLoading: false }));
+      }
+    };
+    fetchData();
+  }, [pageState.page, pageState.pageSize]);
+
   return (
-    <div style={{ height: 600, width: "50%" }}>
-      <DataGrid
-        {...data}
-        initialState={{
-          ...data.initialState,
-          pagination: { paginationModel: { pageSize: 8 } },
-        }}
-        pageSizeOptions={[8, 12, 24]} // aby pageSizeOptions sie pojawiło, pageSize z paginationModel musi być jedną z tych opcji
-      />
-    </div>
+    <Box>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            Server-side Pagination demo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container style={{ marginTop: 100, marginBottom: 100 }}>
+        <DataGrid
+          autoHeight
+          rows={pageState.data}
+          rowCount={pageState.total}
+          loading={pageState.isLoading}
+          rowsPerPageOptions={[10, 30, 50, 70, 100]}
+          pagination
+          page={pageState.page - 1}
+          pageSize={pageState.pageSize}
+          paginationMode="server"
+          onPageChange={(newPage) => {
+            setPageState((old) => ({ ...old, page: newPage + 1 }));
+          }}
+          onPageSizeChange={(newPageSize) =>
+            setPageState((old) => ({ ...old, pageSize: newPageSize }))
+          }
+          columns={columns}
+        />
+      </Container>
+    </Box>
   );
 }
--- src/pages/books/Books.tsx
+++ src/pages/books/Books.tsx
@@ -1,60 +1,100 @@
-import { useState, useEffect, useContext } from "react";
-import axios from "axios";
+import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios"; // Dodajemy import axios
 import { AuthContext, IAuthContext } from "react-oauth2-code-pkce";
 
+const columns = [
+  {
+    field: "title",
+    headerName: "Title",
+    width: 200,
+  },
+  {
+    field: "body",
+    headerName: "Body",
+    flex: 1,
+  },
+];
+
 export const Books = () => {
-  const [books, setBooks] = useState([]);
-  const [totalItems, setTotalItems] = useState(0);
-  const [itemPerSite, setItemPerSite] = useState(25);
-  const [page, setPage] = useState(1);
+  const [pageState, setPageState] = useState({
+    isLoading: false,
+    data: [],
+    total: 0,
+    page: 1,
+    lastPage: 0,
+    pageSize: 5,
+  });
+
   const { token } = useContext<IAuthContext>(AuthContext);
-  const fetchBooks = async () => {
-    try {
-      const response = await axios.get(
-        `https://demo.api-platform.com/admin/books?page=${page}&itemsPerPage=${itemPerSite}`,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-      setBooks(
-        response.data["hydra:member"].map((book, index) => ({
-          ...book,
-          id: index + 1,
-        }))
-      );
-      setTotalItems(response.data["hydra:totalItems"]);
-    } catch (error) {
-      console.error("Error fetching books:", error);
-    }
-  };
 
   useEffect(() => {
-    fetchBooks();
-  }, [page]); // Fetch books whenever page changes
+    const fetchData = async () => {
+      setPageState((old) => ({ ...old, isLoading: true }));
+      try {
+        const response = await axios.get(
+          `https://demo.api-platform.com/admin/books?page=${pageState.page}&itemsPerPage=${pageState.pageSize}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
 
-  const handlePageChange = (newPage) => {
-    setPage(newPage);
-  };
+        setPageState((old) => ({
+          ...old,
+          isLoading: false,
+          data: response.data["hydra:member"].map((book, index) => ({
+            ...book,
+            id: index + 1,
+          })),
+          total: response.data["hydra:totalItems"],
+          lastPage: parseInt(
+            response.data["hydra:view"]["hydra:last"].match(/page=(\d+)/)[1]
+          ),
+        }));
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        setPageState((old) => ({ ...old, isLoading: false }));
+      }
+    };
 
-  const columns = [
-    { field: "id", headerName: "ID", width: 150 },
-    { field: "title", headerName: "Title", width: 250 },
-    { field: "author", headerName: "Author", width: 200 },
-  ];
+    fetchData();
+  }, [pageState.page, pageState.pageSize]);
 
   return (
-    <DataGrid
-      rows={books}
-      columns={columns}
-      paginationMode="server"
-      rowCount={totalItems}
-      pagination
-      autoHeight
-      initialState={{
-        pagination: { paginationModel: { pageSize: 8 } },
-      }}
-      pageSizeOptions={[8, 12, 24]}
-    />
+    <Box>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            Server-side Pagination demo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container style={{ marginTop: 100, marginBottom: 100 }}>
+        <DataGrid
+          autoHeight
+          paginationMode="server"
+          rows={pageState.data}
+          columns={columns}
+          loading={pageState.isLoading}
+          rowCount={pageState.total}
+          pageSizeOptions={[5, 10, 25, 50, 100]}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          onPaginationModelChange={(newPage) => {
+            setPageState((old) => ({
+              ...old,
+              page: newPage.page + 1,
+              pageSize: newPage.pageSize,
+            }));
+          }}
+        />
+      </Container>
+    </Box>
   );
 };
